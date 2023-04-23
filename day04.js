a5a71622b67da24b1b9b14a7fb2f48fcbc48956c
Alex Hernandez <62507090+hernandezaw@users.noreply.github.com>
2023-04-23 04:46:54 -0400

    inicio reto dia 5

---
+++ day04.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// ============================================================================
+// Día Cuatro
+// ============================================================================
+/*
+Santa Claus necesita hacer una revisión de sus cajas de regalos para asegurarse de que puede empaquetarlas todas en su trineo. Cuenta con una serie de cajas de diferentes tamaños, que se caracterizan por su longitud, anchura y altura.
+
+Tu tarea es escribir una función que, dada una lista de cajas con sus tamaños, determine si es posible empaquetar todas las cajas en una sola de manera que cada caja contenga a otra (que a su vez contenga a otra, y así sucesivamente).
+
+Cada caja representa sus medidas con un objeto. Por ejemplo: {l: 2, w: 3, h: 2}. Esto significa que la caja tiene una longitud de 2, una anchura de 3 y una altura de 2.
+
+Una caja entra en otra caja si todos los lados de la primera son menores a los lados de la segunda. Los elfos nos han dicho que las cajas no se pueden rotar, así que no se puede poner una caja de 2x3x2 en una caja de 3x2x2. Veamos unos ejemplos:
+
+fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 }
+]) // true
+En el ejemplo anterior, la caja más pequeña entra en la caja más grande. Por lo tanto, es posible empaquetar todas las cajas en una sola. Ahora veamos un caso que no:
+
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 }
+]
+
+fitsInOneBox(boxes) // false
+*/
+
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 },
+];
+
+function fitsInOneBox(boxes) {
+  const boxesS = boxes.slice();
+  boxesS.sort((a, b) => a.l - b.l);
+  for (let i = 1; i < boxesS.length; i++) {
+    if (!(boxesS[i].l > boxesS[i - 1].l)) return false;
+    if (!(boxesS[i].w > boxesS[i - 1].w)) return false;
+    if (!(boxesS[i].h > boxesS[i - 1].h)) return false;
+  }
+  return true;
+}
+
+console.log(fitsInOneBox(boxes));
